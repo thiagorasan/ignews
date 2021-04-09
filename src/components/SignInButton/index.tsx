c85aabfe190cef78b8a11cc2384add07cc47b372
thiagorasan <trs.tiba@gmail.com>
2021-04-09 01:19:31 -0300

    feature: Home e SignInButton estilizados

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,10 +1,26 @@
 import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+
+import styles from './styles.module.scss';
 
 export function SignInButton() {
-    return (
-        <button type="button">
-            <FaGithub />
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn ? (
+        <button 
+        type="button"
+        className={styles.signInButton}>
+            <FaGithub color="#04d361"/>
+            Thiago Ramos
+            <FiX color="#737380" className={styles.closeIcon}/>
+        </button>
+    ) : (
+        <button 
+        type="button"
+        className={styles.signInButton}>
+            <FaGithub color="#eba417"/>
             Sign in with Github
         </button>
-    )
+    );
 }
