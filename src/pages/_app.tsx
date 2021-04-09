402d426635d0112a07b7efdb5cf4792cdfdc57b8
thiago ramos <trs.tiba@gmail.com>
2021-04-08 22:37:07 -0300

    commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps}  from  'next/app';
+import '../styles/global.scss';
+import { Header } from '../components/Header'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    <Header />
+    <Component {...pageProps} />
+    </>
+  )
+}
+
+  
+
+export default MyApp
